1aa3fbdc9e14dbf0fb1d081149a7566447ef8e4f
kooh-z <kody.j.gigs@gmail.com>
2016-05-18 23:44:20 +0900

    Refactor

--- lib/oden.rb
+++ lib/oden.rb
@@ -1,5 +1,28 @@
 require "oden/version"
 
 module Oden
-  # Your code goes here...
+  require 'open-uri'
+  require 'nokogiri'
+  require 'rubygems'
+  require "FileUtils"
+
+  def self.scrap(url, save_path)
+    scrap_target_page = Nokogiri::HTML(open(url))
+    images_list = Array.new
+    scrap_target_page.css('img.MTMItemThumb').each do |anchor|
+    	images_list.push(anchor[:src])
+      save_image(anchor[:src], save_path)
+    end
+  end
+
+  def self.save_image(url, save_path)
+  	file_name = File.basename(url) + ".jpg"
+  	file_path = save_path + file_name
+  	FileUtils.mkdir_p(save_path) unless FileTest.exist?(save_path)
+  	open(file_path, 'wb') do |output|
+  		open(url) do |data|
+  			output.write(data.read)
+  		end
+  	end
+  end
 end
